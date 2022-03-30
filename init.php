20f20459a121ee35bc3a5a5ab792c5a6d581eed5
celebrater <tkgm45nbchk@gmail.com>
2022-03-31 08:55:55 +0900

    securimage test succeeded at Sakura server, but failed at Xampp every times

--- init.php
+++ init.php
@@ -10,7 +10,7 @@ define('PHBBS_MULTH_PAGE_MODE', false); // 複数のスレッドを使用する�
 define('PHBBS_AVAILABLE', true); // 投稿許可（false で投稿を一時的に制限）
 //define('PHBBS_REPLY_MODE', false); // true でリプライ先のコメントの直下に返事を表示
 define('PHBBS_MAX_COMMENTS', 10); // 1ページあたりのコメント表示数
-define("PHBBS_AUTH", false); // Securimage による画像認証を使用するか
+define("PHBBS_AUTH", true); // Securimage による画像認証を使用するか
 define("PHBBS_PIA_PATH", 'plugins/php_img_auth/'); // php_img_auth
 define("PHBBS_PNLG_PATH", '../common_modules/php_number_link_generator_2/'); // php_number_link_generator
 define("PHBBS_HCM_PATH", '../common_modules/html_common_module.php'); // html_common_module.php
